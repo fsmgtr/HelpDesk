0196fdad7dede6c8c2838d0e090534d2d24fe880
C1322172 Filipe Santos Moutinho <c1322172@interno.bb.com.br>
2022-09-26 22:27:14 -0300

    projeto finalizado

--- helpdesk/src/app/components/nav/nav.component.ts
+++ helpdesk/src/app/components/nav/nav.component.ts
@@ -13,7 +13,7 @@ export class NavComponent implements OnInit {
   constructor(private router: Router, private  autenticacao: AutenticacaoService, private toast: ToastrService) { }
 
   ngOnInit(): void {
-    this.router.navigate(['chamados/update/1'])
+    this.router.navigate(['home'])
   }
 
   logout(){
